156dc03dc27ffea9848f534f154c34f8fe802daa
Anthony Asiimwe <tonyasiimwe14@gmail.com>
2024-10-04 15:57:57 +0300

    Add callback handlers to talk back to parent from child

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,16 @@ const App = () => {
       objectID: 1,
     },
   ];
+
+  const handleSearch = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search />
+      <Search onSearch={handleSearch} />
 
       <hr />
 
@@ -31,11 +36,13 @@ const App = () => {
   );
 };
 
-const Search = () => {
+const Search = (props) => {
   const [searchTerm, setSearchTerm] = React.useState("");
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
+
+    props.onSearch(event);
   };
   return (
     <div>
